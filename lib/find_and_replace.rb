348b15bd99f49e568808fbe5ce7cdd129f8015bd
Bably Kumari and Megan Olson <megan@>
2017-09-06 09:01:15 -0700

    update syntax

--- lib/find_and_replace.rb
+++ lib/find_and_replace.rb
@@ -1,7 +1,7 @@
 public def find_and_replace(string, str1, str2)
   mystring = ""
   if string.include? str1
-  mystring = string.gsub str1, str2
+  mystring = string.gsub(str1, str2) 
   end
   mystring
 end
